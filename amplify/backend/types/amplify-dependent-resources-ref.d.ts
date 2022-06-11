9900591c370d004f20b052a865fbaf729291cd9a
Tu Nguyen <nguyenvantu191298@gmail.com>
2022-06-11 12:28:22 +0700

    BR-01: add amplify backend

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "awsreactsamplec5a62851": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
